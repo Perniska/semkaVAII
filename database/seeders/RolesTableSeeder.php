c45e728ccd9566314b16edb22c335e84df904b7b
Simona Pernišová <smnprnsv@gmail.com>
2024-01-22 00:13:39 +0100

    Your commit message

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB; // Import the DB facade
+
+class RolesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['name' => 'Admin'],
+            ['name' => 'User'],
+            // Add more roles as needed
+        ]);
+    }
+}
